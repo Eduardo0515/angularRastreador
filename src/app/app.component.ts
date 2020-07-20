5bef3163ac1bf48545ec2692f5433912b4cc2e5e
Hugo150 <183382@ids.upchiapas.edu.mx>
2020-07-20 08:16:40 -0500

    First commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {CrudService} from './service/crud.service';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,7 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'rastreadorW';
+
+  constructor(public crudService: CrudService){ }
+
 }
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,17 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyAlDYtum9Txc2zqZaP9GEaF1UFrbHDNrUk",
+    authDomain: "rastreador-ef6fe.firebaseapp.com",
+    databaseURL: "https://rastreador-ef6fe.firebaseio.com",
+    projectId: "rastreador-ef6fe",
+    storageBucket: "rastreador-ef6fe.appspot.com",
+    messagingSenderId: "455383615386",
+    appId: "1:455383615386:web:71b09d614a8e72ba5d1c23",
+    measurementId: "G-BXHWWZM1VC"
+  }
 };
 
 /*
